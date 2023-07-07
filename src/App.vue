fd451742b6538e3e8421680c2a78b60a935d8898
DeepReap <okmmiha@mail.ru>
2023-07-07 14:05:34 +0300

    task done

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
 	<div class="container">
+		<ChangeForm v-if="popupOpen" @cancelClicked="closePopup" @acceptClicked="(value) => changeItem(value)" :item="currentItem" />
 		<ul class="list list-reset">
-			<ChangeForm v-if="popupOpen" @cancelClicked="closePopup" @acceptClicked="(value) => changeItem(value)" :item="currentItem" />
 			<li @click="openPopup(item, index)" v-for="(item, index) of list" class="list__item" :key="item.value + index">
-				<p class="list__item-name">
-					{{ item.name }}
-				</p>
-				<span type="text" class="list__item-input">{{ item.value }}</span>
+				<article class="main-card">
+					<h3 class="main-card__title">{{ item.name }}</h3>
+					<div class="main-card__underline"></div>
+					<p class="main-card__descr">
+						{{ item.value }}
+					</p>
+				</article>
 			</li>
 		</ul>
 	</div>
@@ -37,6 +40,70 @@
 						name: 'foo4',
 						value: 'bar4',
 					},
+					{
+						name: 'foo5',
+						value: 'bar5',
+					},
+					{
+						name: 'foo1',
+						value: 'bar1',
+					},
+					{
+						name: 'foo2',
+						value: 'bar2',
+					},
+					{
+						name: 'foo3',
+						value: 'bar3',
+					},
+					{
+						name: 'foo4',
+						value: 'bar4',
+					},
+					{
+						name: 'foo5',
+						value: 'bar5',
+					},
+					{
+						name: 'foo1',
+						value: 'bar1',
+					},
+					{
+						name: 'foo2',
+						value: 'bar2',
+					},
+					{
+						name: 'foo3',
+						value: 'bar3',
+					},
+					{
+						name: 'foo4',
+						value: 'bar4',
+					},
+					{
+						name: 'foo5',
+						value: 'bar5',
+					},
+					{
+						name: 'foo1',
+						value: 'bar1',
+					},
+					{
+						name: 'foo2',
+						value: 'bar2',
+					},
+					{
+						name: 'foo3',
+						value: 'bar3',
+					},
+					{
+						name: 'foo4',
+						value: 'bar4',
+					},
+					{
+						name: 'foo5',
+						value: 'bar5',
+					},
 				],
 				popupOpen: false,
 				currentItem: null,
@@ -46,15 +113,18 @@
 		methods: {
 			changeItem(changedItem) {
 				this.popupOpen = false;
+				document.body.classList.remove('modal-open');
 				this.list[this.currentItemIndex] = changedItem;
 			},
 			openPopup(item, index) {
 				this.popupOpen = true;
+				document.body.classList.add('modal-open');
 				this.currentItem = item;
 				this.currentItemIndex = index;
 			},
 			closePopup() {
 				this.popupOpen = false;
+				document.body.classList.remove('modal-open');
 			},
 		},
 	};
@@ -63,6 +133,18 @@
 <style>
 	* {
 		box-sizing: border-box;
+	}
+
+	body {
+		min-height: 100vh;
+		margin: 0;
+		padding: 0;
+		font-family: Trebuchet MS;
+		background: #000;
+	}
+
+	body.modal-open {
+		overflow: hidden;
 	}
 
 	.container {
@@ -75,7 +157,7 @@
 	.list {
 		display: flex;
 		flex-wrap: wrap;
-		justify-content: center;
+		justify-content: space-around;
 	}
 
 	.list-reset {
@@ -84,28 +166,47 @@
 	}
 
 	.list__item {
-		padding: 10px;
 		margin-bottom: 20px;
-		border: 1px solid black;
-		width: 100px;
-		height: 100px;
-		background-color: aquamarine;
+		padding: 30px;
+		width: 22%;
+		min-height: 200px;
+		background: #1b1b1b;
+		cursor: pointer;
+		user-select: none;
+		transition: background-color 0.3s ease-in-out;
 	}
 
-	.list__item:not(:last-child) {
-		margin-right: 40px;
+	.list__item:hover {
+		background: #e9204f;
+		box-shadow: #e9204f 0px 5px 15px;
 	}
 
-	.list__item-name {
+	.list__item:not(:nth-child(4n)) {
+		margin-right: 15px;
+	}
+
+	.main-card__title {
+		margin: 40px 0 0 0;
+		font-size: 20px;
+		color: #fff;
+	}
+
+	.main-card__underline {
+		height: 0;
+		width: 35px;
+		border-top: 2px solid #e9204f;
+		margin-top: 10px;
+		transition: border 0.3s ease-in-out;
+	}
+
+	.list__item:hover .main-card__underline {
+		border-top: 2px solid #fff;
+	}
+
+	.main-card__descr {
 		margin: 0;
-		margin-bottom: 20px;
-		text-align: center;
-		font-size: 24px;
-	}
-
-	.list__item-input {
-		margin: 0;
-		text-align: center;
-		width: 100%;
+		margin-top: 15px;
+		color: #fff;
+		font-size: 14px;
 	}
 </style>
--- src/components/ChangeForm.vue
+++ src/components/ChangeForm.vue
@@ -1,9 +1,15 @@
 <template>
-	<div class="container">
-		<p>{{ currentItem.name }}</p>
-		<input type="text" name="" id="" v-model="currentItem.value" />
-		<button @click="acceptClicked">Принять</button>
-		<button @click="cancelClicked">Отмена</button>
+	<div class="overlay">
+		<form class="main-form">
+			<p class="main-form__title">{{ currentItem.name }}</p>
+			<input class="main-form__input" type="text" name="" id="" v-model="currentItem.value" />
+			<div class="main-form__group">
+				<button class="btn btn-reset button_success" @click="acceptClicked">Принять</button>
+			</div>
+			<div class="main-form__group">
+				<button class="btn btn-reset button_cancel" @click="cancelClicked">Отмена</button>
+			</div>
+		</form>
 	</div>
 </template>
 
@@ -28,3 +34,85 @@
 		},
 	};
 </script>
+
+<style>
+	.overlay {
+		position: fixed;
+		overflow: hidden;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100vh;
+		backdrop-filter: blur(10px);
+	}
+
+	.main-form {
+		position: absolute;
+		top: 25%;
+		left: 0;
+		right: 0;
+		padding: 20px;
+		display: flex;
+		flex-direction: column;
+		width: 300px;
+		min-height: 300px;
+		margin-left: auto;
+		margin-right: auto;
+		justify-content: center;
+		background: #1b1b1b;
+		color: #fff;
+		box-shadow: #e9204f 0px 5px 15px;
+	}
+
+	.main-form__title {
+		margin: 40px 0 20px 0;
+		font-size: 20px;
+		color: #fff;
+		user-select: none;
+	}
+
+	.main-form__input {
+		position: relative;
+		display: block;
+		margin-bottom: 20px;
+		padding: 10px 0 10px 20px;
+		height: 45px;
+		border: 0;
+		outline: 0;
+		top: -2px;
+		color: #fff;
+		font-weight: 700;
+		background: rgb(28, 30, 33);
+		box-shadow: inset -100px -100px 0 rgb(28, 30, 33);
+	}
+
+	.main-form__group {
+		display: flex;
+	}
+
+	.button-reset {
+		padding: 0;
+		border: none;
+		background-color: transparent;
+	}
+
+	.btn {
+		display: inline-block;
+		padding: 10px;
+		margin-bottom: 10px;
+		width: 100%;
+		border: none;
+		outline: none;
+		text-align: center;
+		color: #fff;
+		background: #000;
+		cursor: pointer;
+		user-select: none;
+		font-weight: 700;
+		transition: background-color 0.3s ease-in-out, border-color 0.3s ease-in-out;
+	}
+
+	.btn:hover {
+		background-color: #e9204f;
+	}
+</style>
